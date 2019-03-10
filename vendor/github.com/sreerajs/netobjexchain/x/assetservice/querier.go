84b38b487b43c2c1e699785670ec32400302ee99
sreerajs <ssreerajmailbox@gmail.com>
2019-03-10 11:48:43 +0530

    d

--- vendor/github.com/sreerajs/netobjexchain/x/assetservice/querier.go
+++ vendor/github.com/sreerajs/netobjexchain/x/assetservice/querier.go
@@ -31,7 +31,7 @@ func NewQuerier(keeper Keeper) sdk.Querier {
 func queryResolve(ctx sdk.Context, path []string, req abci.RequestQuery, keeper Keeper) (res []byte, err sdk.Error) {
 	name := path[0]
 
-	value := keeper.ResolveAsset(ctx, name)
+	value := keeper.ResolveAsset(ctx, asset)
 
 	if value == "" {
 		return []byte{}, sdk.ErrUnknownRequest("could not resolve asset")
@@ -53,9 +53,9 @@ func queryWhois(ctx sdk.Context, path []string, req abci.RequestQuery, keeper Ke
 
 	whois := Whois{}
 
-	whois.Value = keeper.ResolveAsset(ctx, name)
-	whois.Owner = keeper.GetOwner(ctx, name)
-	whois.Price = keeper.GetPrice(ctx, name)
+	whois.Value = keeper.ResolveAsset(ctx, asset)
+	whois.Owner = keeper.GetOwner(ctx, asset)
+	whois.Price = keeper.GetPrice(ctx, asset)
 
 	bz, err2 := codec.MarshalJSONIndent(keeper.cdc, whois)
 	if err2 != nil {
